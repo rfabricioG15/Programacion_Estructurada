819f2094fece03d2d4f162082c4e5eb2c1d63846
Fabricio Gomez <renielrosa15@gmail.com>
2020-07-09 02:01:11 -0600

    commit inicial

---
+++ src/ProgramacionEstructurada.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <math.h>
+#include <windows.h>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+int main (){
+    setlocale(LC_ALL, "Spanish");
+    cout << "Codigo de prueba" << endl;
+    cout << "Para verificar que lo que haga funcione correctamente" << endl;
+    cout << "Codigo para Github" << endl;
+return 0;
+}
